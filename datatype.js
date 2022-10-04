52b857ccf1a257500b848c0abbccf677df0722d5
Laurel Lao Ang <laurellaoang@gmail.com>
2022-10-04 19:44:53 +0800

    datatypes

---
+++ datatype.js
@@ -0,0 +1,11 @@
+let bool = 4 > 4
+console.log(bool);
+console.log(typeof bool);
+
+let user = "Lao"
+console.log(user);
+console.log(typeof user);
+
+let bigNum = 10002203002n
+console.log(bigNum);
+console.log(typeof bigNum);
