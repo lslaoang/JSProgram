4d86a5efaa4301dd4065e05d82a264244b000e33
Laurel Lao Ang <laurellaoang@gmail.com>
2022-10-06 07:37:21 +0800

    add array impl

--- conversion.js
+++ conversion.js
@@ -14,3 +14,6 @@ let wildcard = number + str
 console.log(typeof wildcard, wildcard);
 let  wildcard2 = 78 + "1222"
 console.log(typeof wildcard2, wildcard2);
+
+let numArray = [1,"122",3]
+console.log(typeof numArray, numArray, numArray[1]);
