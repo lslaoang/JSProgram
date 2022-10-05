9d6bdb28e7abb6f145295b6c7c38eb3f54c0bbee
Laurel Lao Ang <laurellaoang@gmail.com>
2022-10-05 12:51:07 +0800

    string and number

--- conversion.js
+++ conversion.js
@@ -12,3 +12,5 @@ console.log(typeof number, typeof str);
 // implicit conversion
 let wildcard = number + str
 console.log(typeof wildcard, wildcard);
+let  wildcard2 = 78 + "1222"
+console.log(typeof wildcard2, wildcard2);
